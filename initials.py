2a00efcc7e409a76004587f2d99e8a0cf09a1edc
andiramaa <112568792+andiramaa@users.noreply.github.com>
2023-10-26 16:02:32 +0200

    initials.py

---
+++ initials.py
@@ -0,0 +1,9 @@
+# Andi Rama
+# Fun Fact: My sister wanted to name me like the owner in Toy story, my parents put it in the Albanian way, AndI and not AndY :)
+print("  A   RRRR") 
+print(" A A  R   R")
+print("A   A R   R")
+print("AAAAA RRRR")
+print("A   A R  R")
+print("A   A R   R")
+print("A   A R    R")
